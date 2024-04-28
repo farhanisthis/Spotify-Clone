99fd1c02e49bf4976bcabfcfa33c88e441cd9db4
Farhan <farhanisthis@gmail.com>
2024-04-28 21:14:42 +0530

    Configured Dom for Songs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let arijit = document.createElement("div")
+function PlayMusic() {
+    var audio = new Audio("/Spotify-Clone/assets/songs/Apna Bana Le - Arijit Singh.mp3");
+    audio.play();
+  }
+  
+  let songs = document.querySelectorAll(".songs-list");
+  
+  songs.forEach(song => {
+    song.addEventListener("click", function() {
+      console.log("clicked");
+      PlayMusic();
+    });
+  });
+
